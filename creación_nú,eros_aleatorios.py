18116c91255876f3aba982d61b93b650aa67127b
Sofia-M-Luna <sof.munozl@duocuc.cl>
2025-06-06 11:03:56 -0400

    versión3
    Contador de números pares

--- creación_nú,eros_aleatorios.py
+++ creación_nú,eros_aleatorios.py
@@ -2,13 +2,15 @@ import random #librería para llamar y crear números random
 
 numeros = []
 
-for i in range(100): #Ciclo para repetir 100 veces las iteraciones
+for i in range(10): #Ciclo para repetir 100 veces las iteraciones
     n = random.randint(1,100)
     numeros.append(random.randint(1,100)) #Guardamos en la lista el numero
 
-    numeros.sort()
     print(numeros)
 
-#numero = random.randint(1,100) #rango para el numero random
+cont = 0
+for i in range(len(numeros)):
+    if numeros[i]%2==0:
+        cont+=1
 
-    
+print(f"Cantidad de números pares : {cont}")
